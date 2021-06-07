d001d2cf3e8075d06de0b053635e9fccd9fa3c1d
nachumFreedman <nachumyf@gmail.com>
2021-06-07 18:34:48 +0300

    quick fix

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -15,5 +15,13 @@ exports.onRenderBody = ({ setBodyAttributes }, pluginOptions) => {
 }
 // Wraps every page in a component
 exports.wrapPageElement = ({ element, props }) => {
-  return <Layout {...props}>{element}</Layout>
+  return typeof window !== 'undefined'
+  // if window does not exist (its type undefined),
+  // we stop here (since this will be `false` and render nothing)
+  && Layout
+  // ReactMediaPlayer also can be undefined (because of the special loader for html)
+  // we don't want that to render either, so we will add a condition
+  // if it's undefined, it's falsy so it renders `undefined` (nothing)
+  && <Layout {...props}>{element}</Layout>
+  // finally if the two earlier conditions are truthy, let's render!
 }
