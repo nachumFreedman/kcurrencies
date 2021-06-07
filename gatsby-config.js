7c89804de387600b8a7fcf05a4c7f449dc790f97
nachumFreedman <nachumyf@gmail.com>
2021-06-07 19:03:39 +0300

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    title: 'KCurrencies',
+    author: 'Nachum Freedman',
+    description: 'Kosher Currencies',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -15,7 +15,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'kcurrencies.png', // This path is relative to the root of the site.
+        icon: 'kcurrencies.ico', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
